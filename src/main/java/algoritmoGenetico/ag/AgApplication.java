06957121e1715995bb04f618f4dcf2e2a2eb1cc9
Rafaela <rafaelajung12@gmail.com>
2024-10-31 21:03:11 -0300

    feat: Adição planilhas

--- src/main/java/algoritmoGenetico/ag/AgApplication.java
+++ src/main/java/algoritmoGenetico/ag/AgApplication.java
@@ -11,9 +11,30 @@ public class AgApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AgApplication.class, args);
-        String caminhoArquivo = "src/main/resources/planilhas/Curso_CienciaComputacao.xlsx";
-        List<Disciplina> disciplina = FileReaderService.lerHorarios(caminhoArquivo);
-        System.out.println(disciplina.get(0).getProfessor());
+        String caminhoArquivoCC = "src/main/resources/planilhas/Curso_CienciaComputacao.xlsx";
+        List<Disciplina> disciplinaCC = FileReaderService.lerHorarios(caminhoArquivoCC);
+        
+        String caminhoArquivoEM = "src/main/resources/planilhas/Curso_EngenhariaMecanica_Matutino.xlsx";
+        List<Disciplina> disciplinaEM = FileReaderService.lerHorarios(caminhoArquivoEM);
+        
+        String caminhoArquivoEQ = "src/main/resources/planilhas/Curso_EngenhariaQuimica_Matutino.xlsx";
+        List<Disciplina> disciplinaEQ = FileReaderService.lerHorarios(caminhoArquivoEQ);
+        
+        String caminhoArquivoTA = "src/main/resources/planilhas/Curso_TecnicoAdministracaoVespertino_aula3a6a.xlsx";
+        List<Disciplina> disciplinaTA = FileReaderService.lerHorarios(caminhoArquivoTA);
+        
+        String caminhoArquivoTI = "src/main/resources/planilhas/Curso_TecnicoInformaticaParaInternet_Vespertino_aula3a6a.xlsx";
+        List<Disciplina> disciplinaTI = FileReaderService.lerHorarios(caminhoArquivoTI);
+        
+        String caminhoArquivoTM = "src/main/resources/planilhas/Curso_TecnicoMecatronicaVespertino_aula3a6a.xlsx";
+        List<Disciplina> disciplinaTM = FileReaderService.lerHorarios(caminhoArquivoTM);
+        
+        System.out.println(disciplinaCC.get(0).getProfessor());
+        System.out.println(disciplinaEM.get(0).getProfessor());
+        System.out.println(disciplinaEQ.get(0).getProfessor());
+        System.out.println(disciplinaTA.get(0).getProfessor());
+        System.out.println(disciplinaTI.get(0).getProfessor());
+        System.out.println(disciplinaTM.get(0).getProfessor());
     }
 
 }
