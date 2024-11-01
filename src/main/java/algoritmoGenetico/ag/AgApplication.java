88f67c0ae02e99bfca079d2babf273a2a1a5abce
Nathan Will Martins <nathanwillmartins@gmail.com>
2024-11-01 19:21:21 -0300

    feat: Correção das planilhas

--- src/main/java/algoritmoGenetico/ag/AgApplication.java
+++ src/main/java/algoritmoGenetico/ag/AgApplication.java
@@ -18,7 +18,7 @@ public class AgApplication {
         String caminhoArquivoCC = "src/main/resources/planilhas/Curso_CienciaComputacao.xlsx";
         List<Disciplina> disciplinaCC = FileReaderService.lerHorarios(caminhoArquivoCC);
         int[] vetorCC = generateRandomPositionsForClassCode(disciplinaCC, true);
-        
+
         String caminhoArquivoEM = "src/main/resources/planilhas/Curso_EngenhariaMecanica_Matutino.xlsx";
         List<Disciplina> disciplinaEM = FileReaderService.lerHorarios(caminhoArquivoEM);
         int[] vetorEM = generateRandomPositionsForClassCode(disciplinaEM, true);
@@ -26,40 +26,39 @@ public class AgApplication {
         String caminhoArquivoEQ = "src/main/resources/planilhas/Curso_EngenhariaQuimica_Matutino.xlsx";
         List<Disciplina> disciplinaEQ = FileReaderService.lerHorarios(caminhoArquivoEQ);
         int[] vetorEQ = generateRandomPositionsForClassCode(disciplinaEQ, true);
-        
+
         String caminhoArquivoTA = "src/main/resources/planilhas/Curso_TecnicoAdministracaoVespertino_aula3a6a.xlsx";
         List<Disciplina> disciplinaTA = FileReaderService.lerHorarios(caminhoArquivoTA);
         int[] vetorTA = generateRandomPositionsForClassCode(disciplinaTA, true);
-        
+
         String caminhoArquivoTI = "src/main/resources/planilhas/Curso_TecnicoInformaticaParaInternet_Vespertino_aula3a6a.xlsx";
         List<Disciplina> disciplinaTI = FileReaderService.lerHorarios(caminhoArquivoTI);
         int[] vetorTI = generateRandomPositionsForClassCode(disciplinaTI, true);
-        
+
         String caminhoArquivoTM = "src/main/resources/planilhas/Curso_TecnicoMecatronicaVespertino_aula3a6a.xlsx";
         List<Disciplina> disciplinaTM = FileReaderService.lerHorarios(caminhoArquivoTM);
         int[] vetorTM = generateRandomPositionsForClassCode(disciplinaTM, true);
-        
-//        TO FIX: Problema ao ler outras planilhas (só está dando certo com CC)
-//        System.out.println(disciplinaCC.get(0).getProfessor());
-//        System.out.println(disciplinaEM.get(0).getProfessor());
-//        System.out.println(disciplinaEQ.get(0).getProfessor());
-//        System.out.println(disciplinaTA.get(0).getProfessor());
-//        System.out.println(disciplinaTI.get(0).getProfessor());
-//        System.out.println(disciplinaTM.get(0).getProfessor());
+
+        System.out.println(disciplinaCC.get(0).getProfessor());
+        System.out.println(disciplinaEM.get(0).getProfessor());
+        System.out.println(disciplinaEQ.get(0).getProfessor());
+        System.out.println(disciplinaTA.get(0).getProfessor());
+        System.out.println(disciplinaTI.get(0).getProfessor());
+        System.out.println(disciplinaTM.get(0).getProfessor());
     }
-    
+
     public static int[] generateRandomPositionsForClassCode(List<Disciplina> listaAtual, boolean periodoFull) {
         int cromossomos = 0;
         int[] vetorTurmaCodigo = null;
-        
+
         HashSet<Integer> codigosDeAula = new HashSet<>(); //Set para contar os códigos
         Map<Integer, Integer> contarFases = new HashMap<>(); //Map para contar o numero de fases e as vezes que aparecem
-        for (Disciplina d : listaAtual) { 
-            int fase = d.getFase(); 
+        for (Disciplina d : listaAtual) {
+            int fase = d.getFase();
             contarFases.put(fase, contarFases.getOrDefault(fase, 0) + 1);
             codigosDeAula.add(d.getCodigo());
         }
-        
+
         if (periodoFull) {
             cromossomos = generateNumberOfCromossomes(5, contarFases.size());
             vetorTurmaCodigo = new int[cromossomos];
@@ -67,16 +66,15 @@ public class AgApplication {
         } else {
             // Fazer lógica para periodos variados (ex: 3a a 6a)
         }
-        
+
         return vetorTurmaCodigo;
     }
-    
+
     public static int generateNumberOfCromossomes(int dias, int fases) {
         return (dias * 2) * fases;
     }
-    
+
 //    public static int[] randomizeCromossomesValues(int tamanhoVetor, List<Disciplina> listaAtual, int fases) {
 //        
 //    }
-
 }
--- src/main/java/service/FileReaderService.java
+++ src/main/java/service/FileReaderService.java
@@ -20,18 +20,16 @@ public class FileReaderService {
         try (FileInputStream fis = new FileInputStream(caminhoArquivo);
              Workbook workbook = new XSSFWorkbook(fis)) {
 
-            Sheet sheet = workbook.getSheetAt(0); // Primeira aba da planilha
-            for (int i = 2; i <= sheet.getLastRowNum(); i++) { // Começa da linha 1, ignorando cabeçalho
+            Sheet sheet = workbook.getSheetAt(0);
+            for (int i = 2; i <= sheet.getLastRowNum(); i++) {
                 Row row = sheet.getRow(i);
 
-                // Lê cada célula da linha
                 int codigo = (int) row.getCell(0).getNumericCellValue();
                 String disciplina = row.getCell(1).getStringCellValue();
                 int fase = (int) row.getCell(2).getNumericCellValue();
                 int cargaHoraria = (int) row.getCell(3).getNumericCellValue();
                 String professor = row.getCell(4).getStringCellValue();
 
-                // Cria um objeto Disciplina e adiciona à lista
                 Disciplina disc = new Disciplina(codigo, disciplina, fase, cargaHoraria, professor);
                 disciplinas.add(disc);
             }
