9974aeb1667cd464d6368ab5c68485d57244e2bb
CristopherMartarello <crisrossi1313@gmail.com>
2024-11-02 17:16:58 -0300

    feat: Adicionando a lógica de de preenchimento do vetor com os códigos de turma

--- src/main/java/algoritmoGenetico/ag/AgApplication.java
+++ src/main/java/algoritmoGenetico/ag/AgApplication.java
@@ -1,10 +1,12 @@
 package algoritmoGenetico.ag;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import model.Disciplina;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -62,7 +64,7 @@ public class AgApplication {
         if (periodoFull) {
             cromossomos = generateNumberOfCromossomes(5, contarFases.size());
             vetorTurmaCodigo = new int[cromossomos];
-//            vetorTurmaCodigo = randomizeCromossomesValues(cromossomos, listaAtual, contarFases.size());
+            randomizeCromossomesValues(cromossomos, listaAtual, contarFases);
         } else {
             // Fazer lógica para periodos variados (ex: 3a a 6a)
         }
@@ -73,8 +75,34 @@ public class AgApplication {
     public static int generateNumberOfCromossomes(int dias, int fases) {
         return (dias * 2) * fases;
     }
+    
+    public static void randomizeCromossomesValues(int tamanhoVetor, List<Disciplina> listaAtual, Map<Integer, Integer> fases) {
+        List<Integer> chaves = new ArrayList<>(fases.keySet());
+        List<Integer> valores = new ArrayList<>(fases.values());
+       
+        int disciplinaPadding = tamanhoVetor / chaves.size();
+       
+        Random random = new Random();
+        int[] vetorRandomizado = new int[tamanhoVetor];
 
-//    public static int[] randomizeCromossomesValues(int tamanhoVetor, List<Disciplina> listaAtual, int fases) {
-//        
-//    }
+        int contador = 0;
+
+        for (int i = 0; i < chaves.size(); i++) {
+            int valorAtual = valores.get(i);
+
+            // Define os limites para randomização (pegar a partir de acordo com os códigos das fases)
+            int limiteInferior = contador + 1;
+            int limiteSuperior = contador + valorAtual;
+
+            // Preenche com os números aleatórios daquele intervalo
+            for (int j = 0; j < disciplinaPadding && (i * disciplinaPadding + j) < tamanhoVetor; j++) {
+                vetorRandomizado[i * disciplinaPadding + j] = random.nextInt(limiteSuperior - limiteInferior + 1) + limiteInferior;
+            }
+
+            contador = limiteSuperior;
+        }
+       
+        System.out.println(Arrays.toString(vetorRandomizado));  
+    }
+
 }
