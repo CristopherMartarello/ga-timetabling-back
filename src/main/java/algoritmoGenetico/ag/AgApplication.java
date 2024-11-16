c02cef9c2887452a1ad8ff583f4c61371f2b4605
Nathan Will Martins <nathanwillmartins@gmail.com>
2024-11-16 18:58:55 -0300

    feat: Envio de Dados para o Front

--- src/main/java/algoritmoGenetico/ag/AgApplication.java
+++ src/main/java/algoritmoGenetico/ag/AgApplication.java
@@ -16,11 +16,9 @@ import model.ObjetoTabela;
 import model.Professor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import service.FileReaderService;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "controller")
 public class AgApplication {
 
     //Variaveis recebidas pelo front
